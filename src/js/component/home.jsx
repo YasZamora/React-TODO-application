69b99a2260258325d3a9b4eae05beea9eee36630
YasZamora <yzamo001@gmail.com>
2022-02-02 01:13:44 +0000

    mitarea

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,64 @@
-import React from "react";
+import React , {useState} from "react";
+import { ListGroup } from "react-bootstrap";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Lista = () => {
+	const [lista , setLista] = useState(["Wash my hands", "Make the bed", "Wash my hands", "Eat", "Walk the dog"])
+	const FuncionsetLista = () => {
+		setLista([...lista, value]);
+	} 
+	const [value, setValue] = useState("");
+	const handleChange = e => {
+		setValue(e.target.value);
+	};
+	const handleKeypress = e => {
+		if (e.key == "Enter") {
+			FuncionsetLista();
+		}
+	};
+	const deleteItem = (index) => {
+		const listCopy =[...lista]
+		listCopy.splice(index, 1)
+    	setLista(listCopy);
+		
+	}
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	<>	
+	<input value = {value} onChange={handleChange} onKeyPress={handleKeypress}  ></input>	
+	<ListGroup className="grupolista"> 
+	{lista.map((elemento, index) => {
+		return (
+			<>
+			<ListGroup.Item className="Item"> 
+			<span>{elemento}</span> 
+			<span className="x" onClick={() => deleteItem(index)}>x</span>
+			</ListGroup.Item>
+			</>
+		)
+	})}
+	</ListGroup>
+	<div className="contador">
+			
+			{lista.length} item left
+	</div>
+
+	</>
 	);
+};
+
+
+const Home = () => {
+	return(
+	<div className="container">
+		<span className="titulo">todos</span>
+		<Lista />
+	</div>
+	);
+
+	
 };
 
 export default Home;
